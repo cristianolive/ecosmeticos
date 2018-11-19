129500c9d82bfcec82b885701e3932a305788970
cristiano <cristianolive@hotmail.com>
2018-11-19 14:43:23 -0200

    pagseguro

--- checkout/views.py
+++ checkout/views.py
@@ -122,13 +122,16 @@ class PagSeguroView(LoginRequiredMixin, RedirectView):
             Order.objects.filter(user=self.request.user), pk=order_pk
         )
         pg = order.pagseguro()
-        pg.redirect_url = self.request.build_absolute_uri(
-            reverse('checkout:order_detail', args=[order.pk])
-        )
-        pg.notification_url = self.request.build_absolute_uri(
-            reverse('checkout:pagseguro_notification')
-        )
+        pg.redirect_url = "https://ecosmeticos.herokuapp.com/compras/carrinho/"
+        #pg.redirect_url = self.request.build_absolute_uri(
+        #    reverse('checkout:order_detail', args=[order.pk])
+        #)
+        pg.notification_url = "https://ecosmeticos.herokuapp.com/compras/carrinho/"
+        #pg.notification_url = self.request.build_absolute_uri(
+        #    reverse('checkout:pagseguro_notification')
+        #)
         response = pg.checkout()
+        print (response.errors)
         return response.payment_url
 
 

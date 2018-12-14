3121f23cfa0576568aeb66f1b6861ebcdc088759
cristianolive <42304040+cristianolive@users.noreply.github.com>
2018-12-14 13:05:17 -0200

    Media

--- ecosmeticos/urls.py
+++ ecosmeticos/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.views.static import serve as serve_static
+from django.conf.urls.static import static
 from django.contrib.auth.views import login, logout
 
 from core import views

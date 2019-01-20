634d475d85515d9bbee6b1b2cb48228208945721
cristianolive <42304040+cristianolive@users.noreply.github.com>
2019-01-20 13:00:38 -0200

    watson

--- catalog/apps.py
+++ catalog/apps.py
@@ -6,3 +6,7 @@ from django.apps import AppConfig
 class CatalogConfig(AppConfig):
     name = 'catalog'
     verbose_name = 'Catálogo'
+
+    def ready(self):
+        Product = self.get_model('Product')
+        watson.register(Product)

eec417e5a4059e7c97956dfcd47be106b7b4406d
cristianolive <42304040+cristianolive@users.noreply.github.com>
2019-01-20 13:21:37 -0200

    busca e cache

--- catalog/views.py
+++ catalog/views.py
@@ -4,16 +4,30 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
+from django.db import models
+from django.views.decorators.cache import cache_page
+
+from watson import search as watson
+
 from .models import Product, Category
 
 
 class ProductListView(generic.ListView):
 
-    model = Product
+    #model = Product
     template_name = 'catalog/product_list.html'
     context_object_name = 'products'
-    paginate_by = 3
+    paginate_by = 21
 
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        q = self.request.GET.get('q', '')
+        if q:
+            queryset = queryset.filter(
+                models.Q(name__icontains=q) | models.Q(category__name__icontains=q) \
+                | models.Q(description__icontains=q)
+            )
+        return queryset
 
 product_list = ProductListView.as_view()
 
@@ -22,7 +36,7 @@ class CategoryListView(generic.ListView):
 
     template_name = 'catalog/category.html'
     context_object_name = 'product_list'
-    paginate_by = 3
+    paginate_by = 21
 
     def get_queryset(self):
         return Product.objects.filter(category__slug=self.kwargs['slug'])
@@ -35,7 +49,7 @@ class CategoryListView(generic.ListView):
 
 category = CategoryListView.as_view()
 
-
+@cache_page(60 * 10)
 def product(request, slug):
     product = Product.objects.get(slug=slug)
     context = {
